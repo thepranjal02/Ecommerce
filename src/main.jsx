0524760abfcc522fe6c49bdb2d8765f0577505ad
Pranjal <pranjalgangirwal14@gmail.com>
2025-04-27 10:32:38 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
 import { Toaster } from "react-hot-toast";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 createRoot(document.getElementById('root')).render(
